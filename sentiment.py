37eec8f9f92a76d1209e731129d362fdf14678b6
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-16 22:08:22 +0100

    sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,8 +1,22 @@
-import requests
+import requests, os
 
-def fetch_sentiment(symbol):
-    url = f"https://api.coingecko.com/api/v3/search/trending"
-    r = requests.get(url).json()
-    trending = [coin['item']['symbol'].upper() for coin in r['coins']]
-    score = 1 if symbol in trending else 0
-    return score
+def enrich_sentiment(symbol):
+    key = os.getenv("API_KEY_SANTIMENT")
+    url = "https://api.santiment.net/graphql"
+    headers = {"Authorization": f"Bearer {key}"}
+    query = {
+        "query": """
+        {
+          getMetric(metric: "sentiment_positive_total") {
+            timeseriesData(slug: "%s", from: "now-1d", to: "now", interval: "1d") {
+              value
+            }
+          }
+        }
+        """ % symbol.lower()
+    }
+    r = requests.post(url, json=query, headers=headers).json()
+    try:
+        return r["data"]["getMetric"]["timeseriesData"][0]["value"]
+    except:
+        return 0
